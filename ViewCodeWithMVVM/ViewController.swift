b7606d6753d7ae1060487d7505819a766b5e6e62
Matheus Evangelista <matheus.evangelista@g.globo>
2022-08-24 09:22:08 -0300

    Add initial setup

---
+++ ViewCodeWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewCodeWithMVVM
+//
+//  Created by Matheus Evangelista on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
