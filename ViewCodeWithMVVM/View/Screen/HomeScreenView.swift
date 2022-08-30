6165e241a507804bc596e473b0acdfb8f284ad7f
Quellenni Reis <quellennibarreto@gmail.com>
2022-08-29 22:26:54 -0300

    Add HomeScreenView structure

---
+++ ViewCodeWithMVVM/View/Screen/HomeScreenView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeScreenView.swift
+//  ViewCodeWithMVVM
+//
+//  Created by Quellenni Barreto on 29/08/22.
+//
+
+import UIKit
+
+class HomeScreenView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(self.tableView)
+        self.configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setupTableViewProtocols(delegate:UITableViewDelegate, dataSource:UITableViewDataSource){
+        self.tableView.delegate = delegate
+        self.tableView.dataSource = dataSource
+        
+    }
+    
+    private func configConstraints(){
+        NSLayoutConstraint.activate([
+            self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
+        ])
+        
+    }
+   
+    
+
+}
